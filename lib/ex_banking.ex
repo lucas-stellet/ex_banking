ae760473e84606960eea1b6a22e973ced67753f1
Lucas Stellet <lustepe@gmail.com>
2022-11-10 19:59:52 -0300

    refactor send logic

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -101,53 +101,40 @@ defmodule ExBanking do
              | :too_many_requests_to_sender
              | :too_many_requests_to_receiver}
   def send(from_user, to_user, amount, currency) do
-    from_user_operation = Operations.new_transfer(:sender, amount, currency)
-    to_user_operation = Operations.new_transfer(:receiver, amount, currency)
+    with {:ok, sender_transfer_operation} <-
+           validate_and_builds_transfer_operation("sender", from_user, amount, currency),
+         {:ok, receiver_transfer_operation} <-
+           validate_and_builds_transfer_operation("receiver", to_user, amount, currency),
+         {:ok, sender_updated_balance} <-
+           Services.send_money(from_user, sender_transfer_operation),
+         {:ok, receiver_updated_balance} <-
+           Services.receive_money(to_user, receiver_transfer_operation) do
+      Services.finish_operation(from_user, sender_transfer_operation)
+      Services.finish_operation(to_user, receiver_transfer_operation)
 
-    with {:ok, _from_user} <-
-           pack_condition_to_sender(Services.check_account_service_creation(from_user)),
-         {:ok, _to_user} <-
-           pack_condition_to_receiver(Services.check_account_service_creation(to_user)),
-         {{:ok, from_user_transfer_operation}, _from_user} <-
-           pack_condition_to_sender(from_user_operation),
-         {{:ok, to_user_transfer_operation}, _to_user} <-
-           pack_condition_to_receiver(to_user_operation),
-         {:ok, _} <-
-           pack_condition_to_sender(
-             Services.start_operation(from_user, from_user_transfer_operation)
-           ),
-         {:ok, _} <-
-           pack_condition_to_receiver(
-             Services.start_operation(to_user, to_user_transfer_operation)
-           ),
-         {{:ok, sender_balance}, _from_user} <-
-           pack_condition_to_sender(Services.send_money(from_user, from_user_transfer_operation)),
-         {{:ok, receiver_balance}, _from_user} <-
-           pack_condition_to_receiver(Services.receive_money(to_user, to_user_transfer_operation)) do
-      Services.finish_operation(from_user, unpack_user_operation(from_user_operation))
-      Services.finish_operation(to_user, unpack_user_operation(to_user_operation))
-
-      {:ok, sender_balance, receiver_balance}
+      {:ok, sender_updated_balance, receiver_updated_balance}
     else
-      {{:error, :user_does_not_exist}, who} ->
-        {:error, format_error_response_to_person(who, "_does_not_exist")}
-
-      {{:error, :too_many_requests_to_user}, who} ->
-        {:error, format_error_response_to_person(who, "_too_many_requests_to")}
-
-      {{:error, reason}, _} ->
-        Services.finish_operation(from_user, unpack_user_operation(from_user_operation))
-        Services.finish_operation(to_user, unpack_user_operation(to_user_operation))
-        {:error, reason}
+      {:error, _reason} = error ->
+        error
     end
   end
 
-  defp format_error_response_to_person(who, error_message) do
-    String.to_atom(who <> error_message)
+  defp validate_and_builds_transfer_operation(who, user, amount, currency) do
+    user_operation = Operations.new_transfer(String.to_existing_atom(who), amount, currency)
+
+    with :ok <- Services.check_account_service_creation(user),
+         {:ok, user_transfer_operation} <- user_operation,
+         :ok <- Services.start_operation(user, user_transfer_operation) do
+      {:ok, user_transfer_operation}
+    else
+      {:error, :user_does_not_exist} ->
+        {:error, String.to_existing_atom(who <> "_does_not_exist")}
+
+      {:error, :too_many_requests_to_user} ->
+        {:error, String.to_existing_atom("too_many_requests_to_" <> who)}
+
+      {:error, :wrong_arguments} ->
+        {:error, :wrong_arguments}
+    end
   end
-
-  defp unpack_user_operation(packed_user_operation), do: elem(packed_user_operation, 1)
-
-  defp pack_condition_to_sender(condition), do: {condition, "sender"}
-  defp pack_condition_to_receiver(condition), do: {condition, "receiver"}
 end
