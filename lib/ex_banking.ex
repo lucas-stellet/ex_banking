9ae3b3018282d93b1b53776ff0653e92964df4de
Lucas Stellet <lustepe@gmail.com>
2022-11-10 01:27:56 -0300

    creates public api deposit function

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -2,6 +2,7 @@ defmodule ExBanking do
   @moduledoc false
 
   alias ExBanking.Core
+  alias ExBanking.Operations
   alias ExBanking.Services
 
   @spec create_user(user :: String.t()) :: :ok | {:error, :wrong_arguments | :user_already_exists}
@@ -11,6 +12,22 @@ defmodule ExBanking do
       :ok
     else
       error ->
+        error
+    end
+  end
+
+  @spec deposit(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  def deposit(user, amount, currency) do
+    with :ok <- Services.check_account_service_creation(user),
+         {:ok, deposit_operation} <- Operations.new_deposit(amount, currency),
+         :ok <- Services.start_operation(user, deposit_operation),
+         new_balance <- Services.update_balance_account(user, deposit_operation) do
+      Services.finish_operation(user, deposit_operation)
+      {:ok, new_balance}
+    else
+      {:error, _reason} = error ->
         error
     end
   end
