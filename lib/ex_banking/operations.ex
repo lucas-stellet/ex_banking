65cfbe75c77ad2e705e646a32b5b331cfa4a019d
Lucas Stellet <lustepe@gmail.com>
2022-11-10 11:27:02 -0300

    create withdraw operation

--- lib/ex_banking/operations.ex
+++ lib/ex_banking/operations.ex
@@ -1,9 +1,9 @@
 defmodule ExBanking.Operations do
   @moduledoc false
 
-  alias ExBanking.Operations.Deposit
+  alias ExBanking.Operations.{Deposit, Withdraw}
 
-  @type t :: Deposit.t()
+  @type t :: Deposit.t() | Withdraw.t()
 
   @spec new_deposit(amount :: number(), currency :: String.t()) ::
           {:ok, Deposit.t()} | {:error, :wrong_arguments}
@@ -14,6 +14,18 @@ defmodule ExBanking.Operations do
 
       deposit_operation ->
         {:ok, deposit_operation}
+    end
+  end
+
+  @spec new_withdraw(amount :: number(), currency :: String.t()) ::
+          {:ok, Withdraw.t()} | {:error, :wrong_arguments}
+  def new_withdraw(amount, currency) do
+    case Withdraw.new(amount, currency) do
+      :wrong_arguments ->
+        {:error, :wrong_arguments}
+
+      withdraw_operation ->
+        {:ok, withdraw_operation}
     end
   end
 end
