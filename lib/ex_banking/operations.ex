4460c32ab29f558c5335ed8b501360dd863185eb
Lucas Stellet <lustepe@gmail.com>
2022-11-10 12:29:13 -0300

    create balance operation

--- lib/ex_banking/operations.ex
+++ lib/ex_banking/operations.ex
@@ -1,10 +1,9 @@
 defmodule ExBanking.Operations do
   @moduledoc false
 
-  alias ExBanking.Operations.{Deposit, Withdraw}
+  alias ExBanking.Operations.{Balance, Deposit, Withdraw}
 
-  @type t :: Deposit.t() | Withdraw.t()
-
+  @type t :: Deposit.t() | Withdraw.t() | Balance.t()
   @spec new_deposit(amount :: number(), currency :: String.t()) ::
           {:ok, Deposit.t()} | {:error, :wrong_arguments}
   def new_deposit(amount, currency) do
@@ -26,6 +25,18 @@ defmodule ExBanking.Operations do
 
       withdraw_operation ->
         {:ok, withdraw_operation}
+    end
+  end
+
+  @spec new_balance(username :: String.t(), currency :: String.t()) ::
+          {:ok, Balance.t()} | {:error, :wrong_arguments}
+  def new_balance(username, currency) do
+    case Balance.new(username, currency) do
+      :wrong_arguments ->
+        {:error, :wrong_arguments}
+
+      balance_operation ->
+        {:ok, balance_operation}
     end
   end
 end
