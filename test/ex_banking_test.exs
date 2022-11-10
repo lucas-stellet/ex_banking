3a383f77cc7bd85c59f69e3961330a43a4840acf
Lucas Stellet <lustepe@gmail.com>
2022-11-09 22:41:39 -0300

    implement tests for 'create_user' public api function

--- test/ex_banking_test.exs
+++ test/ex_banking_test.exs
@@ -1,8 +1,23 @@
 defmodule ExBankingTest do
-  use ExUnit.Case
-  doctest ExBanking
+  @moduledoc false
 
-  test "greets the world" do
-    assert ExBanking.hello() == :world
+  use ExUnit.Case
+
+  describe "create_user" do
+    test "should returns :ok when the user is created" do
+      assert :ok = ExBanking.create_user("user")
+    end
+
+    test "should returns {:error, :wrong_arguments} when the given param not is a string" do
+      assert {:error, :wrong_arguments} == ExBanking.create_user(123)
+    end
+
+    test "should returns {:error, :user_already_exists} when the user already was created" do
+      user = "user"
+
+      :ok = ExBanking.create_user(user)
+
+      assert {:error, :user_already_exists} == ExBanking.create_user(user)
+    end
   end
 end
