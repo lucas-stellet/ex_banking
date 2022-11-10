61be5a6786e8016950274506e7c27c9c4f181724
Lucas Stellet <lustepe@gmail.com>
2022-11-09 22:24:29 -0300

    add create_account core function and define account arguments validation

--- lib/ex_banking/core.ex
+++ lib/ex_banking/core.ex
@@ -2,4 +2,15 @@ defmodule ExBanking.Core do
   @moduledoc false
 
   alias ExBanking.Core.{Account, Wallet}
+
+  @spec create_account(username :: String.t()) :: {:ok, Account.t()} | {:error, :wrong_arguments}
+  def create_account(username) do
+    case Account.new(username) do
+      :wrong_arguments ->
+        {:error, :wrong_arguments}
+
+      account ->
+        {:ok, account}
+    end
+  end
 end
--- lib/ex_banking/core/account.ex
+++ lib/ex_banking/core/account.ex
@@ -8,10 +8,18 @@ defmodule ExBanking.Core.Account do
           wallets: list(Wallet.t()) | Wallet.t()
         }
 
+  @spec new(username :: String.t()) :: t() | :wrong_arguments
   def new(username) do
-    %__MODULE__{
-      username: username,
-      wallets: Wallet.new()
-    }
+    if is_valid_username?(username) do
+      %__MODULE__{
+        username: username,
+        wallets: Wallet.new()
+      }
+    else
+      :wrong_arguments
+    end
   end
+
+  defp is_valid_username?(username),
+    do: is_binary(username) && true
 end
